5ca2d01e2014858b7e512fd8b11f4c2a142387da
SunPeng <sun557703@hdu.edu.cn>
2020-12-22 14:29:11 +0800

    test commit

---
+++ fs.h
@@ -0,0 +1,12 @@
+//
+// Created by 孙鹏 on 2020/12/22.
+//
+
+#ifndef FILE_SYSTEM_DESIGN_FS_H
+#define FILE_SYSTEM_DESIGN_FS_H
+
+#endif //FILE_SYSTEM_DESIGN_FS_H
+
+#define BLOCKSZIE 1024    // 1KB
+#define SIZE      102400000 // 10000KB
+#define
