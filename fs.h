81f1ddb67f3ceb3fb818909018b13fa37bd1c30d
SunPeng <sun557703@hdu.edu.cn>
2020-12-22 17:33:28 +0800

    new fs.h commit

--- fs.h
+++ fs.h
@@ -7,6 +7,35 @@
 
 #endif //FILE_SYSTEM_DESIGN_FS_H
 
-#define BLOCKSZIE 1024    // 1KB
-#define SIZE      102400000 // 10000KB
-#define
+#define BLOCK_SIZE 1024      //1KB
+#define SIZE 10240000        //10000KB
+#define BLOCK0_NUM 2         //引导块初始占2个盘块
+#define ROOT_BLOCK_NUM 2     //根目录初始占2个盘块
+#define INODE_TABLE_NUM 2    //索引节点共占2个盘块
+#define SUPER_BLOCK_NUM 1    //超级块共占1个盘块
+#define MAX_OPEN_FILE 10     //最多同时打开文件个数
+
+typedef struct BLOCK0
+{
+    int block_size;          //磁盘块大小
+    int block_num;           //磁盘块数量
+    int root;                //根目录的起始盘块号
+    void *inode_table_start; //虚拟磁盘上索引节点开始位置
+    void *root_start;        //虚拟磁盘上根目录开始位置
+    void *data_start;        //虚拟磁盘上数据区开始位置
+    void *super_start;       //虚拟磁盘上超级快开始位置
+    char information[200];   //描述信息
+}block0;
+
+typedef struct USEROPEN
+{
+    char filename[20];
+    int attribute;
+    int access;
+    unsigned short time;
+    unsigned short date;
+    unsigned long length;
+    char dir[80];             //文件打开路径
+    int count;                //读写指针位置
+    bool inodestate;          //索引节点是否修改
+}useropen;
