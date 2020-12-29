248a1a62647d2e4b93923f87b0de3e30e23167b9
sunxiaen <sxe2000@hdu.edu.cn>
2020-12-29 10:56:12 +0800

    fix bug

--- read.c
+++ read.c
@@ -12,10 +12,14 @@ int my_read(int fd,int len){
     
     int id=openlist[fd].direction_chart_id;
     int ori_length=openlist[fd].length;
+    if(ori_length<len){
+        printf("Error [In my_read()]: Read bytes error! Out of range!\n");
+        return 0;
+    }
     int num_block_allocated=openlist[fd].length/BLOCK_SIZE;
     int * begin_addr=virtualDisk+BLOCK_SIZE*id;
 
-    
+
     
     
 
