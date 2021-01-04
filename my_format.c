d6e438b6730d0095aecf1e7de81846e0be970268
SunPeng <76442211+SunP-stack@users.noreply.github.com>
2021-01-04 15:44:08 +0800

    Update my_format.c

--- my_format.c
+++ my_format.c
@@ -82,6 +82,7 @@ void my_format()
     int request_block0_id;
     int super_block_id;
     int inode_table_id;
+    int inode_table_id2;
     int root_id;
 
     request_block0_id = request_block();    //No.0 distribute to block0
@@ -89,8 +90,10 @@ void my_format()
     super_block_id = request_block();       //N0.1 distribute to super_block
     printf("N0.%d distribute to super_block\n",super_block_id);
     inode_table_id = request_block();       //No.2 distribute to inode_table
+    inode_table_id2 = request_block();      //No.3 distribute to inode_table
     printf("No.%d distribute to inode_table\n",inode_table_id);
-    root_id = request_block();              //No.3 distribute to root_dir
+    printf("No.%d distribute to inode_table\n",inode_table_id2);
+    root_id = request_block();              //No.4 distribute to root_dir
     printf("No.%d distribute to root_dir\n\n",root_id);
 
     block0 block0_temp;
@@ -98,6 +101,8 @@ void my_format()
     block0_temp.block_size = BLOCK_SIZE;
     block0_temp.block_num = SIZE/BLOCK_SIZE;
     block0_temp.root = root_id;
+    block0_temp.super_block = super_block_id;
+    block0_temp.inode_table = inode_table_id;
     block0_temp.root_start = virtualDisk + root_id*BLOCK_SIZE;
     block0_temp.super_start = virtualDisk + super_block_id*BLOCK_SIZE;
     block0_temp.inode_table_start = virtualDisk + inode_table_id*BLOCK_SIZE;
@@ -106,6 +111,8 @@ void my_format()
     block0_point = virtualDisk;
     printf("block0.block_size is %d\n",block0_point->block_size);
     printf("block0.block_num is %d\n",block0_point->block_num);
+    printf("block0.super_block is %d\n",block0_point->super_block);
+    printf("block0.inode_table is %d\n",block0_point->inode_table);
     printf("block0.root is %d\n",block0_point->root);
     printf("block0.super_start is %p\n",block0_point->super_start);
     printf("block0.inode_table_start is %p\n",block0_point->inode_table_start);
