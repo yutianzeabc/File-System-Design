6c1e23d77b56e8f42c14741db87db9124b705f01
SunPeng <76442211+SunP-stack@users.noreply.github.com>
2021-01-08 17:04:40 +0800

    Update group_link_operator.c

--- group_link_operator.c
+++ group_link_operator.c
@@ -7,29 +7,23 @@
 
 #include <string.h>
 #include "fs.h"
+#include "c_operator.h"
 
-int request_block()
-{
+int request_block() {
     int *temp = free_block_stack;
     int count = *temp;
     int result;
     S_free--;
     result = *S_free;
-    if (count > 1)
-    {
+    if (count > 1) {
         count--;
         *temp = count;
         return result;
-    }
-    else if (count==1)
-    {
-        if(result == 999)
-        {
+    } else if (count == 1) {
+        if (result == 999) {
             return -1;
-        }
-        else
-        {
-            memcpy(free_block_stack, virtualDisk+result*1024, sizeof(char)*1024);
+        } else {
+            memcpy(free_block_stack, virtualDisk + result * 1024, sizeof(char) * 1024);
             temp = free_block_stack;
             count = *temp;
             S_free = temp + count + 1;
@@ -38,20 +32,16 @@ int request_block()
     }
 }
 
-void recycle_block(int id)
-{
+void recycle_block(int id) {
     int *temp = free_block_stack;
     int count = *temp;
-    if (count<250)
-    {
+    if (count < 250) {
         *S_free = id;
         count++;
         S_free++;
         *temp = count;
-    }
-    else if (count==250)
-    {
-        memcpy(virtualDisk+id*1024, free_block_stack, sizeof(char)*1024);
+    } else if (count == 250) {
+        memcpy(virtualDisk + id * 1024, free_block_stack, sizeof(char) * 1024);
         S_free = free_block_stack;
         *S_free = 1;
         S_free++;
@@ -59,6 +49,7 @@ void recycle_block(int id)
         S_free++;
     }
 }
+
 #endif //FILE_SYSTEM_DESIGN_FS_GROUP_LINK_OPERATOR
 /*
 
