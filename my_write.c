874f0ab5fd38a8656047440d0a922c9cd71e0223
sunxiaen <sxe2000@hdu.edu.cn>
2021-01-09 02:40:49 +0800

    finish mode2 & mode0,1 passed all testcase

--- my_write.c
+++ my_write.c
@@ -170,32 +170,111 @@ int my_write(int fd,int mode){
         memcpy(cursor_addr+last_pointer,str+str_cursor,waiting_len*sizeof(char));
         USEROPENS[fd].length+=str_len;//更新USEROPENS中长度
         printf("%d B added!",str_len);
+        return 0;
 
     }else if(mode==2){//插入覆盖写
-        int pos;
-        printf("[mode:2] I Please input the insert position: ");
-        scanf("%d",&pos);
-        getchar();
-        USEROPENS[fd].count=pos;
-        int continue_b=USEROPENS[fd].count;//插入指针在总字节的位置 0开始
-        if(continue_b+1>ori_length){
-            printf("Error [In my_write() mode 'c']: Position of continue bytes error!\n");
+        if(USEROPENS[fd].length==0){
+            printf("Error [In my_write() mode 'I']: Blank File! Cannot Cover!\n");
             return -1;
         }
-        char str[BLOCK_SIZE*128];
+        int pos;
+        printf("[mode:2] I\n");
+        printf("Please input the insert position: ");
+        fflush(stdin);
+        scanf("%d",&pos);
+        getchar();
+        
+        
+        if(pos>ori_length||pos<0){
+            printf("Error [In my_write() mode 'I']: Position of continue bytes error!\n");
+            return -1;
+        }
+        USEROPENS[fd].count=pos;
+        int continue_b=USEROPENS[fd].count;//插入指针在总字节的位置 (计算从0开始)
+
+        char str[BLOCK_SIZE*128]={'\0'};
+        fflush(stdin);
         scanf("%[^\n]",str);
         getchar();
         int str_len=strlen(str);
-        int total_len=+continue_b;
+        if(str_len==0){
+            printf("Exit [In my_write() 'I']: Blank input. Return!\n");
+            return -1;
+        }
+        //total_len作用域
+        int total_len=continue_b+str_len;
         if(total_len>128*BLOCK_SIZE){
-            printf("Warning [In my_write() mode 'c']:OverFlow! Will Cut down the tail!\n");
+            printf("Warning [In my_write() mode 'I']:OverFlow! Will Cut down the tail!\n");
             str_len=BLOCK_SIZE*128-continue_b;
         }
+        //total_len作用域结束
+
         //此时的str_len为最终写入长度
         //此时的continue_b为最终写入开始位置
+        char *init_addr=NULL;
+        char *cursor_addr=NULL;
+
         int waiting_len=str_len;
+        int this_time_write_len=0;
+        //cnt指示文件下一盘块（未分配，待命）
+        int cnt=0;
+        if(ori_length%BLOCK_SIZE==0) {
+            cnt=num_block_allocated;
+        }
+        else cnt=num_block_allocated+1;
+        
+        int block_num=0;
+        //insert_block_num 指示文件要写入盘块号
+        int insert_block_num=continue_b/BLOCK_SIZE;
+        int insert_pointer=continue_b%BLOCK_SIZE;
+       
+        int str_cursor=0;
+        while((waiting_len+insert_pointer)>BLOCK_SIZE){
+            // int pysical_num=(begin_addr+(2*insert_block_num+1)*sizeof(int));
+            if(insert_block_num>=cnt){
+                int new_blocknum=request_block();//申请
+                *(int *)(begin_addr+(2*insert_block_num)*sizeof(int))=insert_block_num;//写到文件索引表 第一列
+                *(int *)(begin_addr+(2*insert_block_num+1)*sizeof(int))=new_blocknum;//写到文件索引表 第二列
+                // insert_block_num=insert_block_num+1;
+                if(new_blocknum==-1){
+                    printf("Error [In my_write() mode 'a']:Try to allocate one block for writing, but failed!\n");
+                    return -1;
+                }
+            }
+            int cursor_blocknum=*((int *)(begin_addr+(2*insert_block_num+1)*sizeof(int)));
 
+            cursor_addr=virtualDisk+BLOCK_SIZE*cursor_blocknum;
+            memcpy(cursor_addr+insert_pointer,str+str_cursor,sizeof(char)*(BLOCK_SIZE-insert_pointer));
+            waiting_len=waiting_len-(BLOCK_SIZE-insert_pointer);
+            str_cursor=str_cursor+(BLOCK_SIZE-insert_pointer);
+            insert_pointer=0;
+            insert_block_num=insert_block_num+1;
+        }
+        if(insert_block_num>=cnt){
+            int new_blocknum=request_block();//申请
+            *(int *)(begin_addr+(2*insert_block_num)*sizeof(int))=insert_block_num;//写到文件索引表 第一列
+            *(int *)(begin_addr+(2*insert_block_num+1)*sizeof(int))=new_blocknum;//写到文件索引表 第二列
+            // insert_block_num=insert_block_num+1;
+            if(new_blocknum==-1){
+                printf("Error [In my_write() mode 'a']:Try to allocate one block for writing, but failed!\n");
+                return -1;
+            }
+        }
+        int cursor_blocknum=*((int *)(begin_addr+(2*insert_block_num+1)*sizeof(int)));
+        cursor_addr=virtualDisk+BLOCK_SIZE*cursor_blocknum;
+        memcpy(cursor_addr+insert_pointer,str+str_cursor,sizeof(char)*(waiting_len));
+        
 
+        printf("%d B inserted from pos %d!\n",str_len,pos);
+        if(USEROPENS[fd].length>=pos+str_len){
+            printf("File length not increase!\n");
+           
+        }else{
+            printf("File length increased %d B!\n",pos+str_len-USEROPENS[fd].length);
+            USEROPENS[fd].length=pos+str_len;//更新USEROPENS中长度
+        }
+        
+        return 0;
     }else{
         return -1;
     }
